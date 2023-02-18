336e3150de06e94711c67ec154605e760542f944
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-18 17:03:27 +0500

    replacedNumber

--- MatrixElement/MatrixElement/Program.cs
+++ MatrixElement/MatrixElement/Program.cs
@@ -6,63 +6,63 @@ namespace MatrixElement
         static void Main(string[] args)
         {
             int row = 10;
-            int col = 10;
-            int[,] matrix = new int[row, col];
-            int min = 1;
-            int max = 1000;
-            int maxElement = 0;
-            int zero = 0;
+            int column = 10;
+            int[,] matrixNumbers = new int[row, column];
+            int minNumber = -1000;
+            int maxNumber = 0;
+            int maxElement = int.MinValue;
+            int replacedByNumber = 0;
 
             Random random = new Random();
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < matrixNumbers.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrixNumbers.GetLength(1); j++)
                 {
-                    matrix[i, j] = random.Next(min, max);
+                    matrixNumbers[i, j] = random.Next(minNumber, maxNumber);
                 }
             }
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < matrixNumbers.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrixNumbers.GetLength(1); j++)
                 {
-                    if (matrix[i, j] > maxElement)
+                    if (matrixNumbers[i, j] > maxElement)
                     {
-                        maxElement = matrix[i, j];
+                        maxElement = matrixNumbers[i, j];
                     }
                 }
             }
 
             Console.WriteLine($"Max element = {maxElement} \n");
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < matrixNumbers.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrixNumbers.GetLength(1); j++)
                 {
-                    Console.Write(matrix[i, j] + "\t");
+                    Console.Write(matrixNumbers[i, j] + "\t");
                 }
                 Console.WriteLine("\n");
             }
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < matrixNumbers.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrixNumbers.GetLength(1); j++)
                 {
-                    if (matrix[i, j] == maxElement)
+                    if (matrixNumbers[i, j] == maxElement)
                     {
-                        matrix[i, j] = zero;
+                        matrixNumbers[i, j] = replacedByNumber;
                     }
                 }
             }
 
             Console.WriteLine();
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < matrixNumbers.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrixNumbers.GetLength(1); j++)
                 {
-                    Console.Write(matrix[i, j] + "\t");
+                    Console.Write(matrixNumbers[i, j] + "\t");
                 }
                 Console.WriteLine("\n");
             }
